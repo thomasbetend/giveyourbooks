5d1c20d5ba211d11e32344113b582d989f51af7b
thomasbetend <thomas.betend@yahoo.fr>
2023-03-09 09:39:05 +0100

    problme with liip

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Knp\Bundle\GaufretteBundle\KnpGaufretteBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];

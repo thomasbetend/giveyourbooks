5e18b377b204464937d9adcbc37e56caac8a3cc6
thomasbetend <thomas.betend@yahoo.fr>
2023-10-30 08:09:18 +0100

    adress controller fixing

--- assets/controllers/address_controller.js
+++ assets/controllers/address_controller.js
@@ -7,8 +7,6 @@ export default class extends Controller {
     ];
 
     connect() {
-      console.log('youhou');
-
       new window.Autocomplete(this.autocompleteTarget.id, {
         selectFirst: true,
         insertToInput: true,
@@ -62,59 +60,10 @@ export default class extends Controller {
 
             this.latitudeTarget.value = latitude;
             this.longitudeTarget.value = longitude;
-        //   // remove all layers from the map
-        //   map.eachLayer(function (layer) {
-        //     if (!!layer.toGeoJSON) {
-        //       map.removeLayer(layer);
-        //     }
-        //   });
-      
-        //   const { display_name } = object.properties;
-        //   const [lng, lat] = object.geometry.coordinates;
-        //   // custom id for marker
-      
-        //   const marker = L.marker([lat, lng], {
-        //     title: display_name,
-        //   });
-      
-        //   marker.addTo(map).bindPopup(display_name);
-      
-        //   map.setView([lat, lng], 8);
         },
       
-        // get index and data from li element after
-        // hovering over li with the mouse or using
-        // arrow keys ↓ | ↑
-        // onSelectedItem: ({ index, element, object }) => {
-        //   console.log("onSelectedItem:", { index, element, object });
-        // },
-      
-        // the method presents no results
-        // no results
         noResults: ({ currentValue, template }) =>
           template(`<li>No results found: "${currentValue}"</li>`),
       });
-      
-      // --------------------------------------------------
-      
-    //   // MAP PART
-    //   const config = {
-    //     minZoom: 4,
-    //     maxZoom: 18,
-    //   };
-    //   // magnification with which the map will start
-    //   const zoom = 3;
-    //   // coordinates
-    //   const lat = 52.22977;
-    //   const lng = 21.01178;
-      
-    //   // calling map
-    //   const map = L.map("map", config).setView([lat, lng], zoom);
-      
-    //   // Used to load and display tile layers on the map
-    //   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    //     attribution:
-    //       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    //   }).addTo(map);
     }
 }
